5bfd24c15e8078ff89237246e1a733f1685ae4f8
Programmator24 <nikita@gmail.com>
2022-07-09 15:59:46 +1000

    add Fill array method

---
+++ Task_000/Program.cs
@@ -0,0 +1,18 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк,
+// длинна которых либо меньше либо равняется 3 символам.  
+
+Console.WriteLine("Введите колличество слов");
+int number = int.Parse(Console.ReadLine());
+
+void FillArray(string[] str, int number)
+{
+    Console.WriteLine($"Введите {number} слов через Enter:");
+        for (int i = 0; i < str.Length; i++)
+            {
+                str[i] = Console.ReadLine();
+            }
+            
+}
+
+string[] strings = new string [number];
+FillArray(strings, number);
