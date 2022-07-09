2b4e5c0f91d312b2e656aadc1d426cb153b5117b
Programmator24 <nikita@gmail.com>
2022-07-09 16:02:55 +1000

    add Sort Array Method

--- Task_000/Program.cs
+++ Task_000/Program.cs
@@ -14,5 +14,19 @@ void FillArray(string[] str, int number)
             
 }
 
+void SortArray(string[] str)
+{
+    for (int i = 0; i < str.Length; i++)
+    {
+        if (str[i].Length < 4)
+        {
+            Console.WriteLine(str[i]);
+        }
+    }
+}
+
 string[] strings = new string [number];
 FillArray(strings, number);
+
+Console.WriteLine();
+SortArray(strings);
